34b48c5ea72cdb419c99c7d3b67df54d772f0437
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-16 17:28:48 -0300

    editando arquivo FavoriteFragment

--- app/src/main/java/com/luizeduardobrandao/mybooks/ui/FavoriteFragment.kt
+++ app/src/main/java/com/luizeduardobrandao/mybooks/ui/FavoriteFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.luizeduardobrandao.mybooks.databinding.FragmentDashboardBinding
 import com.luizeduardobrandao.mybooks.viewmodels.FavoriteViewModel
@@ -18,13 +19,14 @@ class FavoriteFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    val favoriteViewModel: FavoriteViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val favoriteViewModel =
-            ViewModelProvider(this).get(FavoriteViewModel::class.java)
+
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
