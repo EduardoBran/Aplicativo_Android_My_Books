8372a0f957ce585d2ab9be5552d07dd6c52d5aa9
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-16 17:34:59 -0300

    removendo metodo observavel de FavoriteFragment que sera criado posteriormente

--- app/src/main/java/com/luizeduardobrandao/mybooks/ui/FavoriteFragment.kt
+++ app/src/main/java/com/luizeduardobrandao/mybooks/ui/FavoriteFragment.kt
@@ -28,13 +28,8 @@ class FavoriteFragment : Fragment() {
 
 
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        favoriteViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
