d286f7361d46c759e4e7b3aab791ef869103ed9f
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-17 12:09:48 -0300

    implementacao da recycler view da home parte 4 - criando em BookViewHolder metodo ainda vazio para implementacao dos atributos (livros)

--- app/src/main/java/com/luizeduardobrandao/mybooks/ui/viewholder/BookViewHolder.kt
+++ app/src/main/java/com/luizeduardobrandao/mybooks/ui/viewholder/BookViewHolder.kt
@@ -2,6 +2,11 @@ package com.luizeduardobrandao.mybooks.ui.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
 import com.luizeduardobrandao.mybooks.databinding.ItemBookBinding
+import com.luizeduardobrandao.mybooks.entity.BookEntity
 
 class BookViewHolder(private val item: ItemBookBinding): RecyclerView.ViewHolder(item.root) {
+
+    fun bind(book: BookEntity){
+        // código para obter os valores de BookEntity
+    }
 }
