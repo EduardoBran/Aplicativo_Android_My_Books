041f98761dd9408ce59f6b63b908d33ffea311d2
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-17 12:15:36 -0300

    implementacao da recycler view da home parte 5 - implementado em BookAdapter as chamadas para a funcao metodo de implementacao de atributos de BookViewHolder

--- app/src/main/java/com/luizeduardobrandao/mybooks/ui/adapter/BookAdapter.kt
+++ app/src/main/java/com/luizeduardobrandao/mybooks/ui/adapter/BookAdapter.kt
@@ -1,19 +1,29 @@
 package com.luizeduardobrandao.mybooks.ui.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.luizeduardobrandao.mybooks.databinding.ItemBookBinding
+import com.luizeduardobrandao.mybooks.entity.BookEntity
 import com.luizeduardobrandao.mybooks.ui.viewholder.BookViewHolder
 
 class BookAdapter: RecyclerView.Adapter<BookViewHolder>() {
+
+    private val bookList = mutableListOf<BookEntity>()
+
+    // É responsável por criar o elemento de layout
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
-        TODO("Not yet implemented")
+        val view = ItemBookBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BookViewHolder(view)
     }
 
+    // Retorna quantos elementos existem na Recycler View
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return bookList.size
     }
 
+    // É responsável por atribuir/obter os valores dos atributos (livros) para o layout
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(bookList[position])
     }
 }
