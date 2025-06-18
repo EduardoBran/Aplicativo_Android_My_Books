df7a03877dde60f3e6fede7b7e8ebbefc3915ea3
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-18 14:39:39 -0300

    adicionando metodo para remover livro em DetailsFragment e DetailsViewModel ainda sem atualizar a listagem de livros para o usuario

--- app/src/main/java/com/luizeduardobrandao/mybooks/ui/DetailsFragment.kt
+++ app/src/main/java/com/luizeduardobrandao/mybooks/ui/DetailsFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.luizeduardobrandao.mybooks.R
 import com.luizeduardobrandao.mybooks.viewmodels.DetailsViewModel
 import com.luizeduardobrandao.mybooks.databinding.FragmentDetailsBinding
@@ -51,9 +53,9 @@ class DetailsFragment : Fragment() {
     }
 
     // Observa o LiveData exposto pelo ViewModel através de "getBooksId()" de "DetailsViewModel"
-    private fun setObservers(){
+    private fun setObservers() {
         // itens que serão observados
-        viewModel.book.observe(viewLifecycleOwner){
+        viewModel.book.observe(viewLifecycleOwner) {
             // o que acontece quando receber o id
             binding.textviewTitle.text = it.title
             binding.textviewAuthorValue.text = it.author
@@ -62,43 +64,78 @@ class DetailsFragment : Fragment() {
 
             // chamando função para trocar cor das tags de genero
             setGenreBackground(it.genre)
+
+            // chamando função para remover livro
+            viewModel.bookRemove.observe(viewLifecycleOwner) {
+                if (it) {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.msg_removed_successfully),
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    // Retonar a mesma página após confirmação de remoção ou não remoção
+                    requireActivity().supportFragmentManager.popBackStack()
+                }
+            }
         }
     }
 
     // Função para reagir aos listeners
-    private fun setListeners(){
+    private fun setListeners() {
 
         // implementando lógica do botão de voltar (também serve para arrastar o dedo para voltar)
-        binding.imageviewBack.setOnClickListener{
+        binding.imageviewBack.setOnClickListener {
             requireActivity().supportFragmentManager.popBackStack()
         }
+
+        // Botão "Remover livro" usando função "handleMove()"
+        binding.buttonRemove.setOnClickListener { handleRemove() }
     }
 
-    // Lógica para alterar cor das tags de genero
-    private fun setGenreBackground(genre: String){
+    // Função para remover livro usando AlertDialog e função "deleteBook()" de "DetailsViewModel"
+    private fun handleRemove() {
+        val builder = AlertDialog.Builder(requireContext())
 
-        if (genre == "Aventura"){
+        // definir comportamento
+        builder.setMessage(getString(R.string.dialog_message_delete_item))
+            .setPositiveButton(getString(R.string.dialog_positive_button_yes)) { dialog, which ->
+                viewModel.deleteBook(bookId)
+            }
+            .setNegativeButton(getString(R.string.dialog_negative_button_no)) { dialog, which ->
+                dialog.dismiss()
+            }
+
+        // Exibir o dialog
+        builder.create().show()
+    }
+
+
+    // Lógica para alterar cor das tags de genero
+    private fun setGenreBackground(genre: String) {
+
+        if (genre == "Aventura") {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_aventura)
         }
-        if (genre == "Contos"){
+        if (genre == "Contos") {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_contos)
         }
-        if (genre == "Distopia"){
+        if (genre == "Distopia") {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_distopia)
         }
-        if (genre == "Fantasia"){
+        if (genre == "Fantasia") {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_fantasy)
         }
-        if (genre == "Ficção"){
+        if (genre == "Ficção") {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_ficcao)
         }
-        if (genre == "Infantil"){
+        if (genre == "Infantil") {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_infantil)
         }
-        if (genre == "Mistério"){
+        if (genre == "Mistério") {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_misterio)
         }
-        if (genre == "Romance"){
+        if (genre == "Romance") {
             binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_romance)
         }
     }
--- app/src/main/java/com/luizeduardobrandao/mybooks/viewmodels/DetailsViewModel.kt
+++ app/src/main/java/com/luizeduardobrandao/mybooks/viewmodels/DetailsViewModel.kt
@@ -15,9 +15,18 @@ class DetailsViewModel : ViewModel() {
     private val _book = MutableLiveData<BookEntity>()
     val book: LiveData<BookEntity> = _book
 
+    // variável para atribuir o valor do booleano para remoção de um livro
+    private val _bookRemove = MutableLiveData<Boolean>()
+    val bookRemove: LiveData<Boolean> = _bookRemove
+
     // Função para recuperar o valor passado por HomeFragment (id do livro) usando função
     // "getBooksById" implementada em "BookRepository"
     fun getBooksById(id: Int) {
         _book.value = repository.getBookById(id)
+    }
+
+    // Função para excluir um livro da lista
+    fun deleteBook(id: Int){
+        _bookRemove.value = repository.deleteBook(id)
     }
 }
