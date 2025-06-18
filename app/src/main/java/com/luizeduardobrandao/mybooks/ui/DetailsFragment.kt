638b41d530ede7125e9d63840ad2b5c88cc59de9
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-18 12:04:29 -0300

    adicionando logica para alterar cor das tags de genero em DetailsFragmet

--- app/src/main/java/com/luizeduardobrandao/mybooks/ui/DetailsFragment.kt
+++ app/src/main/java/com/luizeduardobrandao/mybooks/ui/DetailsFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.luizeduardobrandao.mybooks.R
 import com.luizeduardobrandao.mybooks.viewmodels.DetailsViewModel
 import com.luizeduardobrandao.mybooks.databinding.FragmentDetailsBinding
 import com.luizeduardobrandao.mybooks.helper.BookConstants
@@ -58,6 +59,9 @@ class DetailsFragment : Fragment() {
             binding.textviewAuthorValue.text = it.author
             binding.textviewGenreValue.text = it.genre
             binding.checkboxFavorite.isChecked = it.favorite
+
+            // chamando função para trocar cor das tags de genero
+            setGenreBackground(it.genre)
         }
     }
 
@@ -67,6 +71,35 @@ class DetailsFragment : Fragment() {
         // implementando lógica do botão de voltar (também serve para arrastar o dedo para voltar)
         binding.imageviewBack.setOnClickListener{
             requireActivity().supportFragmentManager.popBackStack()
+        }
+    }
+
+    // Lógica para alterar cor das tags de genero
+    private fun setGenreBackground(genre: String){
+
+        if (genre == "Aventura"){
+            binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_aventura)
+        }
+        if (genre == "Contos"){
+            binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_contos)
+        }
+        if (genre == "Distopia"){
+            binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_distopia)
+        }
+        if (genre == "Fantasia"){
+            binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_fantasy)
+        }
+        if (genre == "Ficção"){
+            binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_ficcao)
+        }
+        if (genre == "Infantil"){
+            binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_infantil)
+        }
+        if (genre == "Mistério"){
+            binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_misterio)
+        }
+        if (genre == "Romance"){
+            binding.textviewGenreValue.setBackgroundResource(R.drawable.rounded_label_romance)
         }
     }
 }
