d7b7a223ec6acbf1e157c687fced347ce3635167
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-30 15:20:47 -0300

    adicionando trecho de código para possivel realizacao de migracao do banco de dados

--- app/src/main/java/com/luizeduardobrandao/mybooks/repository/BookDatabase.kt
+++ app/src/main/java/com/luizeduardobrandao/mybooks/repository/BookDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.luizeduardobrandao.mybooks.entity.BookEntity
 
 @Database(entities = [BookEntity::class], version = 1)
@@ -27,6 +29,22 @@ abstract class BookDatabase: RoomDatabase() {
                 }
             }
             return instance
+        }
+    }
+
+    // Migrações
+    private object Migrations {
+        /**
+         * Atualização de versão de banco de dados
+         */
+        val migrationFromV1ToV2: Migration = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                // Limpar o banco de dados
+                db.execSQL("DELETE FROM Book")
+
+                // Criar uma nova coluna, atualizar valores, etc etc
+                // Qualquer tipo de lógica de banco de dados para garantir a atualização de versão
+            }
         }
     }
 }
